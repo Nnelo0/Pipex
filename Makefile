f056616cc58d109e4221427db4aef003d59f4f91
Nnelo0 <broudicelouann@gmail.com>
2024-12-19 19:42:50 +0100

    V1

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = pipex
+CC = cc
+CFLAGS = -Wall -Werror -Wextra
+SRCS = pipex.c \
+
+LIBFTDIR = libft
+LIBFT = $(LIBFTDIR)/libft.a
+
+all: $(LIBFT) $(NAME)
+
+$(NAME): $(SRCS) $(LIBFT)
+	$(CC) -I. -I$(LIBFTDIR) $(SRCS) $(LIBFT) $(CFLAGS) -o $(NAME)
+	@echo "Compiled $(NAME) successfully!"
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFTDIR)
+
+clean:
+	$(MAKE) -C $(LIBFTDIR) clean
+
+fclean: clean
+	rm -f $(NAME) $(NAME_BONUS)
+	$(MAKE) -C $(LIBFTDIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ pipex.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnelo <nnelo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 17:55:46 by nnelo             #+#    #+#             */
+/*   Updated: 2024/12/19 19:38:24 by nnelo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void	free_cmd(char **cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i] != NULL)
+	{
+		free(cmd[i]);
+		i++;
+	}
+	free(cmd);
+}
+
+static void	close_all(int files[2], int fd[2])
+{
+	close(fd[0]);
+	close(fd[1]);
+	close(files[0]);
+	close(files[1]);
+}
+
+static void	write_in_pipe(int files[2], int fd[2], char **argv, char **envp)
+{
+	char	**cmd1;
+	char	*path;
+
+	dup2(files[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close_all(files, fd);
+	cmd1 = ft_split(argv[2], ' ');
+	path = ft_strjoin("/bin/", cmd1[0]);
+	execve(path, cmd1, envp);
+	write(2, "Execve failed for cmd1\n", 23);
+	free_cmd(cmd1);
+	free(path);
+	exit(1);
+}
+
+static void	write_in_file(int files[2], int fd[2], char **argv, char **envp)
+{
+	char	**cmd2;
+	char	*path;
+
+	dup2(fd[0], STDIN_FILENO);
+	dup2(files[1], STDOUT_FILENO);
+	close_all(files, fd);
+	cmd2 = ft_split(argv[3], ' ');
+	path = ft_strjoin("/bin/", cmd2[0]);
+	execve(path, cmd2, envp);
+	write(2, "Execve failed for cmd2\n", 23);
+	free_cmd(cmd2);
+	free(path);
+	exit(1);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		files[2];
+	int		fd[2];
+	pid_t	pid;
+
+	if (argc != 5)
+		exit(ft_printf("Usage: ./pipex infile cmd1 cmd2 outfile\n"));
+	if (pipe(fd) == -1)
+		exit(ft_printf("pipe error\n"));
+	if (access(argv[1], F_OK) == -1)
+		exit(ft_printf("%s: No such file\n", argv[1]));
+	if (access(argv[1], R_OK) == -1)
+		exit(ft_printf("%s: permission denied\n", argv[1]));
+	pid = fork();
+	files[0] = open(argv[1], O_RDONLY);
+	files[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (files[0] == -1 || files[1] == -1 || pid == -1)
+		exit(ft_printf("failed open file or fork error\n"));
+	if (pid == 0)
+		write_in_pipe(files, fd, argv, envp);
+	if (fork() == 0)
+		write_in_file(files, fd, argv, envp);
+	close_all(files, fd);
+	wait(NULL);
+	wait(NULL);
+	return (0);
+}
